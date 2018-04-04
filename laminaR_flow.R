ece571ac6d41a62c06b9ed3fd49a2d7bd71ea78c
eugejoh <ejoh90@gmail.com>
2018-04-04 02:09:06 -0400

    output edit

--- laminaR_flow.R
+++ laminaR_flow.R
@@ -2,14 +2,15 @@
 # Reynolds number (Re) < 2100 in horizontal pipe
 # Hagen-Poiseuille's Law
 # Qv = (pi*R^4*Pdelta) / (8*mu*l)
+
 # flow rate is proportional to pressure drop, pipe radius (^4)
 #     inversely proportional to viscosity, and pipe length
 
+# Packages ####
 library(pracma) #load practical math package
 library(ggplot2) #data viz
 library(scales) #plot scales
 library(dplyr) #data wrangling
-library(purrr) #functional programming
 library(viridis) #data viz palette
 library(animation) #animation
 library(gganimate) #ggplot animation
@@ -25,22 +26,23 @@ mu <- 8.94e-4 # visocity for water. units: [Pa*s] at temp = 25 C
 R <- 0.1 #radius of pipe [m]
 l <- 4 #length of pipe [m]
 
-# time dependent
-time <- seq(0,19) #length 20
-Pit <- 0.2*time*Pi
-Pft <- 0.2*time*Pf
-Pdiff <- abs(Pft-Pit)
-
 # boundary conditions
 N <- 100 #radial resolution
 r <- seq(-R,R,length.out=N) #diameter of pipe
 S <- pi*(R^2) #area of pipe axial
 Q <- Vmean*S #volumetric flow rate
 
-# Create Data Viz
-# plot shows V (average speed) and r (pipe radius)
-((Pdiff)*(R^2)/(4*mu*l))*(1-(r^2)/R^2) #eqn for V
+# time dependent
+time <- seq(0,19) #length 20
+Pit <- 0.2*time*Pi 
+Pft <- 0.2*time*Pf
+Pdiff <- abs(Pft-Pit) #pressure difference over time
 
+
+
+# Create Data Viz ####
+# plot shows V (average speed) and r (pipe radius)
+# ((Pdiff)*(R^2)/(4*mu*l))*(1-(r^2)/R^2) #eqn for V
 out1 <- list()
 for (i in seq_along(Pdiff)) {
   out1[[i]] <- ((Pdiff[i])*(R^2)/(4*mu*l))*(1-(r^2)/R^2)
@@ -57,11 +59,14 @@ df_t2 <- cbind(df_t,r) %>%
 
 p1 <- ggplot(data=df_t2, aes(y=r,x=V,col=Qv,frame=time,cumulative=TRUE)) +
   geom_path() + geom_point(alpha=0.05) +
+  scale_x_continuous(breaks=pretty_breaks()) +
   scale_colour_viridis(option = "C", discrete = FALSE) +
-  theme_dark() +
-  labs(title = "Hagen-Poiseuille's Law: Velocity Profile t =",
-       y="Pipe Radius (m)", x = "Velocity (m/s)")
+  theme_dark() + theme(axis.title = element_text(size=14),
+                       axis.text = element_text(size=12),
+                       plot.title = element_text(size=16,face = "bold")) +
+  labs(title = "Hagen-Poiseuille's Law: Velocity Profile t =", 
+       y = "Pipe Radius (m)", x = "Velocity (m/s)")
 p1
 
-gganimate(p1,filename = "Vprofile.gif", title_frame = TRUE,
-          interval=0.2, ani.width = 900, ani.height = 350)
+gganimate(p1,filename = "Vprofile1.gif", title_frame = TRUE,
+          interval=0.2, ani.width = 1100, ani.height = 400)
