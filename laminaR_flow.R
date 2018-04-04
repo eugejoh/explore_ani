29a2d3491c25e0d2eb01420ea4a8289f66e4f30c
eugejoh <ejoh90@gmail.com>
2018-04-04 09:03:22 -0400

    add plot

--- laminaR_flow.R
+++ laminaR_flow.R
@@ -20,23 +20,21 @@ library(gganimate) #ggplot animation
 Pi <- 10 #initial pressure [Pa]
 Pf <- 50 #final pressure [Pa]
 
-## Boundary Conditions ####
+## Boundary Conditions #
 # Pipe and fluid characteristics
 mu <- 8.94e-4 #viscosity for water. units: [Pa*s] at temp = 25 C
 R <- 0.1 #radius of pipe [m]
 l <- 4 #length of pipe [m]
 
-# boundary conditions
 N <- 100 #radial resolution
 r <- seq(-R,R,length.out=N) #diameter of pipe
 S <- pi*(R^2) #area of pipe axial
 
-# time dependent
+# Pressure-time dependence
 time <- seq(0,19) #length 20
 Pit <- 0.2*time*Pi 
 Pft <- 0.2*time*Pf
 Pdiff <- abs(Pft-Pit) #pressure difference over time
-
 
 
 # Create Data Viz ####
